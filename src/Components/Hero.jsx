195fd13cfbf569f47e6d195b2f3eb8902fad263d
priyanshu kumar <dlnpriyanshu@gmail.com>
2024-10-13 13:51:14 +0530

    minor ui changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import "../Css/Hero.css";
 import { motion } from 'framer-motion';
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebookMessenger } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import logo from "../assets/logo.png"
 
 export default function Hero() {
   return (
     <div className="hero-container">
-      <div className="background-color" />
+
+      {/* <div className='w-full flex justify-center items-center h-[30vh]'><img src={logo} alt="" className='w-[120px] absolute z-10'/></div> */}
       <motion.div className="hero"
         initial={{ opacity: 0 }} 
         whileInView={{ opacity: 1 }}
@@ -15,13 +22,17 @@ export default function Hero() {
       >
           <p className='w-full'>NIT JAMSHEDPUR</p>
           <h1 className='text-white z-[2] text-[40px] font-semibold animate-pulse'>presents</h1>
-      <div className='text-white mb-4 text-[20px] font-medium drop-shadow-[0_0_5px_black] animate-bounce absolute bottom-0 text-center'>|<br/>Scroll down</div>
-      {/* <div className='absolute text-white z-10 bottom-1 left-1 flex flex-col gap-3'>
-        <div className='lg:w-10 w-8 aspect-square bg-white rounded-full'></div>
-        <div className='lg:w-10 w-8 aspect-square bg-white rounded-full'></div>
-        <div className='lg:w-10 w-8 aspect-square bg-white rounded-full'></div>
-        <div className='lg:w-10 w-8 aspect-square bg-white rounded-full'></div>
-      </div> */}
+
+      <div className='text-white mb-4 z-10 text-[20px] font-medium drop-shadow-[0_0_5px_black] animate-bounce absolute bottom-0 text-center'>|<br/>Scroll down</div>
+      
+      <div className='absolute text-white z-10 lg:bottom-10 lg:left-10 md:bottom-7 md:left-7 sm:bottom-4 sm:left-4 bottom-2 left-2   flex flex-col gap-5'>
+        <div className='text-center text-[50px] '>|</div>
+       <FaLinkedin className='text-[28px] hover:text-blue-400 cursor-pointer'/>
+       <FaInstagramSquare className='text-[28px] hover:text-pink-500 cursor-pointer'/>
+       <FaFacebookMessenger className='text-[28px] hover:text-blue-400 cursor-pointer'/>
+       <FaYoutube className='text-[28px] hover:text-red-500 cursor-pointer'/>
+       <FaSquareXTwitter className='text-[28px] hover:text-blue-400 cursor-pointer'/>
+      </div>
       </motion.div>
 
    
