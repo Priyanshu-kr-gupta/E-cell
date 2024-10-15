a360287ef12f5fdec3cadf2a400b3ace9373a294
ayush kumar <ayush2422005@gmail.com>
2024-10-15 19:13:51 +0530

    lazy loading update

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../Css/Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <span className="letter">E</span>
+      <span className="letter">C</span>
+      <span className="letter">E</span>
+      <span className="letter">L</span>
+      <span className="letter">L</span>
+    </div>
+  );
+};
+
+export default Loader;
